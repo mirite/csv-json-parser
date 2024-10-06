a307fda4a389d6fdff4906bea88b10e5632a64ef
Jesse Conner <me@jesseconner.ca>
2024-10-06 10:08:15 -0400

    Test with line breaks

--- src/tests/parsing.rs
+++ src/tests/parsing.rs
@@ -12,4 +12,10 @@ mod tests {
         let result = parse_csv_string("A,\"B, E\",C\n1,D,3");
         assert_eq!(result, "[{\"A\":1,\"B, E\":\"D\",\"C\":3}]");
     }
+
+    #[test]
+    fn read_with_breaks() {
+        let result = parse_csv_string("A,B,C\n1,\"D\nE\",3");
+        assert_eq!(result, "[{\"A\":1,\"B\":\"D\nE\",\"C\":3}]");
+    }
 }
