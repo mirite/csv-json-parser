93708da4e23d61087beaf281001781601adbbdcc
Jesse Conner <me@jesseconner.ca>
2024-10-09 18:42:38 -0400

    Formatting and docs

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+import { general } from "@mirite/eslint-config-mirite";
+
+export default [
+  ...general,
+  {
+    ignores: ["pkg/**","**/pkg/**"],
+  },
+];
--- ts/index.ts
+++ ts/index.ts
@@ -1,11 +1,19 @@
-import wasm, {parse_string} from "../pkg/csv_json_parser.js";
+import wasm, { parse_string } from "../pkg/csv_json_parser.js";
 
-export async function parseString<T extends object>(data: string): Promise<T|null> {
-    try {
-    await wasm()
-    return JSON.parse(parse_string(data));
-    } catch (e) {
-        console.error(e);
-        return null;
-    }
+/**
+ *
+ * @param data
+ */
+export async function parseString<T extends object>(
+  data: string,
+): Promise<T | null> {
+  try {
+    await wasm();
+    const str = parse_string(data);
+    console.log(str);
+    return JSON.parse(str);
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 }
